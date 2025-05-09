263df0273b9e843253d9d5af07911396dede0b48
Lucio <lfurmann6@gmail.com>
2025-05-09 23:05:41 +0200

    leaving multiplayer resets background

--- Scripts/Multiplayer/multiplayer_ranking.gd
+++ Scripts/Multiplayer/multiplayer_ranking.gd
@@ -38,4 +38,4 @@ func _on_play_again_pressed():
 func _on_leave_pressed():
 	Client.reset_multiplayer_connection()
 	Client.active = false
-	GlobalFunctions.load_menu("main")
+	GlobalFunctions.load_menu("main", true, true)
